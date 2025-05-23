5079356b6de37eab99505f146d6f94dd405e2ae4
ASHAN2021 <akh8130@gmail.com>
2025-05-24 00:14:39 +0530

    tab bar, home screen and chat page first implement

---
+++ chatapp/lib/Pages/chat_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ChatPage extends StatefulWidget {
+  const ChatPage({super.key});
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: const Color(0xff128C7E),
+        child: const Icon(Icons.chat),
+      ),
+    );
+  }
+}
